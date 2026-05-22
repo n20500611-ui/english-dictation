055d952950a5ad6a14864c0249f01b6d855821c7
n20500611-ui <n20500611@gmail.com>
2026-05-23 01:02:41 +0800

    Update files

---
+++ docs/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'english-dictation-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/vocabulary.js'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event - serve from cache, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
